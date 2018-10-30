d01bf1062722820b4ba26302228701797c03eb28
rbsolis <bob.solis@gmail.com>
2018-10-30 09:14:47 +0100

    Created wrapper function to upsert metadata.

---
+++ db/common/dml/urbo_upsertmetadata.sql
@@ -0,0 +1,144 @@
+--
+-- Copyright 2017 Telefónica Digital España S.L.
+--
+-- This file is part of UrboCore API.
+--
+-- UrboCore API is free software: you can redistribute it and/or
+-- modify it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- UrboCore API is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+-- General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with UrboCore API. If not, see http://www.gnu.org/licenses/.
+--
+-- For those usages not covered by this license please contact with
+-- iot_support at tid dot es
+--
+
+/*
+* General function to upsert metadata information at general level or for an specific scope.
+
+--------------------------------------------------------------------------------
+HOW TO USE:
+PgSQL:
+SELECT urbo_upsertmetadata(json);
+
+json Example:
+{
+  "schools.institute.position": {
+      "var_name" : "Posición del instituto"
+    },
+  "irrigation.humiditysensor": {
+      "table_name": "irrigation_humiditysensor",
+      "mandatory" : true
+    },
+  "dumps": {
+      "config": { "carto": { "account": "cedus-admin" } },
+      "nodata": false
+    }
+}
+Call Example:
+SELECT urbo_upsertmetadata('{"schools.institute.position":{"var_name":"Posición del instituto"},"irrigation.humiditysensor":{"table_name":"irrigation_humiditysensor","mandatory":true},"dumps":{"config":{"carto":{"account":"cedus-admin"}},"nodata":false}}'::json);
+
+----------------------------------------
+Category complete json definition
+
+{
+  "category_name": -- example: "dumps"
+  {
+    "id_scopes": array of strings, -- if not defined it is applied generally and for all the existing scopes.
+    "id_category": character varying(255), -- NOT NULL
+    "category_name": character varying(255),
+    "nodata": boolean, -- DEFAULT false
+    "config": jsonb -- DEFAULT ''{}''::jsonb
+  }
+}
+
+----------------------------------------
+Entity complete json definition
+
+{
+  "category_name.entity_name": -- example: "irrigation.humiditysensor"
+  {
+    "id_scopes": array of strings, -- if not defined it is applied generally and for all the existing scopes.
+    "id_entity": character varying(255), -- NOT NULL
+    "entity_name": character varying(255),
+    "id_category": character varying(255),
+    "table_name": character varying(255),
+    "mandatory": boolean, -- DEFAULT false
+    "editable": boolean -- DEFAULT false
+  }
+}
+
+----------------------------------------
+Variable complete json definition
+
+{
+  "category_name.entity_name.variable_name": -- example: "schools.institute.position"
+  {
+    "id_scopes": array of strings, -- if not defined it is applied generally and for all the existing scopes.
+    "id_variable": character varying(255), -- NOT NULL
+    "id_entity": character varying(255), -- NOT NULL
+    "entity_field": character varying(255), -- NOT NULL
+    "var_name": character varying(255),
+    "var_units": character varying(255),
+    "var_thresholds": double precision[],
+    "var_agg": character varying[],
+    "var_reverse": boolean,
+    "config": jsonb,
+    "table_name": character varying(255),
+    "type": character varying(255), -- DEFAULT ''catalogue''::character varying
+    "mandatory": boolean, -- DEFAULT false
+    "editable": boolean -- DEFAULT false
+  }
+}
+--------------------------------------------------------------------------------
+
+*/
+
+
+
+
+
+
+DROP FUNCTION IF EXISTS urbo_upsertmetadata(json);
+
+CREATE OR REPLACE FUNCTION urbo_upsertmetadata(
+  _json_defs json DEFAULT '{}'::json
+)
+RETURNS void
+AS $$
+  DECLARE
+    _json_object json;
+    _key_json text;
+  BEGIN
+    -- Iterate
+    SELECT ('{"schools.institute.position":{"var_name":"Posición del instituto"},"irrigation.humiditysensor":{"table_name":"irrigation_humiditysensor","mandatory":true},"dumps":{"config":{"carto":{"account":"cedus-admin"}},"nodata":false}}')::json into _json_object;
+
+    FOR _key_json IN SELECT * FROM json_object_keys(_json_object)
+    LOOP
+      -- Check regex
+      RAISE NOTICE 'key %', _key_json;
+      IF (_key_json ~* '^(.*?)\.(.*?)\.(.*?)$') THEN
+        RAISE NOTICE 'it is a variable';
+        RAISE NOTICE 'adfdf: %', _json_object::json->_key_json;
+      ELSIF (_key_json ~* '^(.*?)\.(.*?)$') THEN
+        RAISE NOTICE 'it is a entity';
+      ELSE
+        RAISE NOTICE 'it is a category';
+      END IF;
+
+    END LOOP;
+
+  END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+select urbo_upsertmetadata();
