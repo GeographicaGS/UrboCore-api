375d0adf424be0c8ac4299a0115c800b96803a66
rbsolis <bob.solis@gmail.com>
2018-11-12 07:37:32 +0100

    Added user_scope_password to scope table

--- db/common/urbo_metadata.sql
+++ db/common/urbo_metadata.sql
@@ -1,21 +1,21 @@
 --
 -- Copyright 2017 Telefónica Digital España S.L.
--- 
+--
 -- This file is part of UrboCore API.
--- 
+--
 -- UrboCore API is free software: you can redistribute it and/or
 -- modify it under the terms of the GNU Affero General Public License as
 -- published by the Free Software Foundation, either version 3 of the
 -- License, or (at your option) any later version.
--- 
+--
 -- UrboCore API is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 -- General Public License for more details.
--- 
+--
 -- You should have received a copy of the GNU Affero General Public License
 -- along with UrboCore API. If not, see http://www.gnu.org/licenses/.
--- 
+--
 -- For those usages not covered by this license please contact with
 -- iot_support at tid dot es
 --
@@ -124,6 +124,7 @@ CREATE OR REPLACE FUNCTION urbo_metadata(
           parent_id_scope character varying(255) DEFAULT NULL::character varying,
           status smallint DEFAULT 0,
           timezone character varying(255),
+          user_scope_password character varying(255) DEFAULT NULL::character varying,
           config jsonb
       );
 
